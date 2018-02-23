f2e352348b3d5cdde99edd5fe548b77ef7b40df2
Shin, Yong-Jun <yshin1209@gmail.com>
2018-02-22 23:11:17 -0500

    Working PID

--- EaaS/WebAPI/Controllers/PIDController.cs
+++ EaaS/WebAPI/Controllers/PIDController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using PID.Interfaces;
 
 namespace WebAPI.Controllers
 {
@@ -11,5 +14,15 @@ namespace WebAPI.Controllers
     [Route("api/PID")]
     public class PIDController : Controller
     {
+        [HttpGet]
+        [Route("{id}/{measuredValue}/{setPoint}/{Kp}/{Ki}/{Kd}")]
+        public async Task<string> Get(int id, double measuredValue, double setPoint, double Kp, double Ki, double Kd)
+        {
+            ActorId actorId = new ActorId(id);
+            var actor1 = ActorProxy.Create<IPID>(actorId, new Uri("fabric:/Application/PIDActorService"));
+            double response = await actor1.RunPIDAsync(measuredValue, setPoint, Kp, Ki, Kd);
+            return response.ToString();
+        }
+
     }
 }
