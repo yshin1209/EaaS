e8e1adc902e01c06dcb2042d58684c61e9f14910
yshin1209 <yshin1209@gmail.com>
2018-03-11 01:11:18 -0500

    Add files via upload

--- LMS_client.m
+++ LMS_client.m
@@ -1,14 +1,17 @@
 % LMS (Least Mean Squares) Adaptive Parameter Estimation web service client
 % Yong-Jun Shin (2018)
+clear all
+close all
 
 N = 100;                       % number of iteration
 x = zeros(N,1);                % output signal x
 u = 10*ones(N,1) + randn(N,1); % input signal u
+parameter = 0.837;             % parameter value
 response = zeros(N,3);         % web service response [estiated output, estimation error, estimated parameter]
 newServiceId = '1';            % new Actor service ID
 timeDelay = 1;                 % time dealy between the input and the output
 timeDelay_value = num2str(timeDelay);  % convert timeDelay into string
-stepSize = 0.01;              % LMS step size
+stepSize = 0.001;             % LMS step size
 stepSize_value = num2str(stepSize);    % convert stepSize into string
 % initialize LMS web servie with newServiceId and timeDelay_value
 url = ['http://csmlab8.uconn.edu/api/lms/' newServiceId '/' timeDelay_value];
@@ -16,7 +19,7 @@ webread(url);
 
 for n = 2:N
     % simulated x which takes input signal u as an input
-    x(n) = 0.847*u(n-timeDelay) + randn(1,1);
+    x(n) = parameter*u(n-timeDelay) + randn(1,1);
     
     % convert x and r into string
     x_value = num2str(x(n));
@@ -36,4 +39,7 @@ plot(1:N, x, 'b', 1:N, estOutput, 'g', 1:N, estError, 'r', 1:N, estParameter, '*
 legend('x (output)', 'estimated x', 'estimation error', 'estimated parameter');
 xlabel('time(n)');
 ylabel('magnitude (arbitrary unit)');
-axis([0 N 0 20]);
+parameter_value = num2str(parameter);
+txt1 = ['true parameter value = ' parameter_value] ;
+text(30,3,txt1);
+axis([0 N -5 15]);
