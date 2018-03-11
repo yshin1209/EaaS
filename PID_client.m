79acf8a40a971757539beb83c814bb3ae275dd2f
yshin1209 <yshin1209@gmail.com>
2018-03-11 00:03:08 -0500

    Add files via upload

--- PID_client.m
+++ PID_client.m
@@ -1,3 +1,5 @@
+% PID (Proportional-Integral_Derivative) web service client
+% Yong-Jun Shin (2018)
 N = 63;                     % number of iteration
 x = zeros(N,1);             % output (measured) signal x
 u = zeros(N,1);             % control signal u
@@ -7,23 +9,21 @@ Kp = num2str(2);            % the proportional parameter Kp
 Ki = num2str(2);            % the integral parameter Ki
 Kd = num2str(0.8);          % the derivative parameter Kd
 
-for n = 2:N
+for n= 3:N
     % add disturbance (d) to x at time > 50
-    if n > 50 
-        d = 60; 
-    else
-        d = 0;
+    if n> 50 d=60; 
+    else d=0;
     end
     
     % simulated x which takes control signal u as an input
-    x(n) = 0.4*u(n-1)+0.6*x(n-1)+d; 
+    x(n) =0.4*u(n-1)+0.6*x(n-1) + d; 
     
     % convert x and r into string
     x_value = num2str(x(n));
     r_value = num2str(r(n));
     
     % reset the stored values (on the cloud) to zero in the beginning
-    if n == 2 
+    if n==3 
         reset_value = 'true'; 
     else
         reset_value = 'false';
@@ -39,4 +39,4 @@ plot(1:N, r, 'r', 1:N, x, 'b', 1:N, u, 'g');
 legend('r (reference)', 'x (output)', 'u (control)');
 xlabel('time(n)');
 ylabel('magnitude (arbitrary unit)');
-axis([0 63 0 450]);
+axis([0 N 0 450]);
