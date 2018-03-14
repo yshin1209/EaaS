c9a856a4396da4e448dd2c7609a6e4aae553a26b
yshin1209 <yshin1209@gmail.com>
2018-03-14 14:55:26 -0400

    Update PID_client.m

--- PID_client.m
+++ PID_client.m
@@ -10,15 +10,17 @@ newServiceId = '1';         % new Actor service ID
 Kp = num2str(2);            % the proportional parameter Kp
 Ki = num2str(2);            % the integral parameter Ki
 Kd = num2str(0.8);          % the derivative parameter Kd
-timeCom = zeros(N,1);   % thetime it takes for the round-trip between the client and the cloud
+timeCom = zeros(N,1);       % time for the round-trip between the client and the cloud
 
 for n= 2:N
     % add disturbance (d) to x at time > 50
-    if n> 50 d=60; 
-    else d=0;
+    if n > 50 
+        d=60; 
+    else
+        d=0;
     end
     
-    % simulated x which takes control signal u as an input
+    % simulate x which takes control signal u as an input
     x(n) =0.4*u(n-1)+0.6*x(n-1) + d; 
     
     % convert x and r into string
@@ -38,14 +40,14 @@ for n= 2:N
     u(n) = webread(url); 
     timeCom(n) = toc; % time for the round-trip between the client and the cloud
 end
-
+subplot(2,1,1);
 plot(1:N, r, 'r', 1:N, x, 'b', 1:N, u, 'g');
 legend('r (reference)', 'x (output)', 'u (control)');
 xlabel('time(n)');
 ylabel('magnitude (arbitrary unit)');
 axis([0 N 0 450]);
 
-figure
+subplot(2,1,2);
 plot (1:N, timeCom, '*')
-xlabel('time(n)');
-ylabel('time for the round-trip between the client and the cloud');
+xlabel('time (n)');
+ylabel('round-trip time (second)');
