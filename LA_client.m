96e25d059671c13e5f77e86865399598627dbe3d
yshin1209 <yshin1209@gmail.com>
2018-04-02 18:06:59 -0400

    Create LA_client.m

---
+++ LA_client.m
@@ -0,0 +1,45 @@
+% BLAS 1
+% Vector-Vector Multiplication
+
+clc;
+clear all;
+
+id = 1;
+N = 10;
+vector1 = rand(N,1);
+vector2 = rand(N,1);
+url = 'http://csmlab8.uconn.edu/api/la/blas1';
+data = struct('Id', id, 'Vector1', vector1, 'Vector2', vector2);
+options = weboptions('MediaType','application/json');
+response = webwrite (url, data, options);
+
+%%
+% BLAS 2
+% Matrix-Vector Multiplication
+clc;
+clear all;
+
+id = 1;
+N = 10;
+matrix = rand(N,N);
+vector = rand(N,1);
+url = 'http://csmlab8.uconn.edu/api/la/blas2';
+data = struct('Id', id, 'Matrix', matrix, 'Vector', vector);
+options = weboptions('MediaType','application/json');
+response = webwrite (url, data, options);
+
+
+%%
+% BLAS 3
+% Matrix-Matrix Multiplication
+clc;
+clear all;
+
+id = 1;
+N = 10
+matrix1 = rand(N,N);
+matrix2 = rand(N,N);
+url = 'http://csmlab8.uconn.edu/api/la/blas3';
+data = struct('Id', id, 'Matrix1', matrix1, 'Matrix2', matrix2);
+options = weboptions('MediaType','application/json');
+response = webwrite (url, data, options)
