50f05697617bf4857e31f86967debeb69ce28a48
Shin, Yong-Jun <yshin1209@gmail.com>
2018-04-09 19:05:22 -0400

    Added Actor ActorService

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPCMath;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vector x = new Vector(5, Math.PI);
+            Vector y = Vector.Random(5, -100.0, 1000.0);
+            //Vector z;
+            //z = x + y;
+
+            //Cnsl.Show("x", x);
+            Cnsl.Show("y", y);
+            //Cnsl.Show("z", z);
+
+            Cnsl.ReadLine();
+        }
+    }
+}
---
+++ EaaS/ClassLibrary/VecScaOpClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class VecScaOpClass
+    {
+        public int Id { get; set; }
+        public double[] Vector { get; set; }
+        public double Scalar { get; set; }
+    }
+}
